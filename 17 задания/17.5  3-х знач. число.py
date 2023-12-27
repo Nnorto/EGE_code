922320c141111a9a781824b93776a4bd83a2a72f
artemzaharov <artem4ikaz@icloud.com>
2023-12-27 15:10:13 +0300

    Решение

--- 17 задания/17.5  3-х знач. число.py
+++ 17 задания/17.5  3-х знач. число.py
@@ -17,3 +17,24 @@ for i in range(len(a) - 1):
             count += 1
 
 print(count, max_sum)
+
+'''
+
+a = [int(s) for s in open("17 файлы/17_5.txt")]
+count = 0
+min_5 = 333343333232334432
+max_sum = 0
+for x in range(len(a)):
+    if a[x] % 10 == 5 and len(str(a[x])) == 3:
+        min_5 = min(a[x], min_5)
+
+for i in range(len(a) - 1):
+    if ((99 < a[i] < 1000) + (99 < a[i + 1] < 1000)) == 1:
+        summa = a[i] + a[i + 1]
+        if summa % min_5 == 0:
+            max_sum = max(max_sum, summa)
+            count += 1
+
+print(count, max_sum)
+
+'''

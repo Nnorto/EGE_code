ce43d48cf7a33e3d3e1341ba4ef5246498e35931
artemzaharov <artem4ikaz@icloud.com>
2023-12-27 14:28:40 +0300

    Решение

--- 17 задания/17.3 тройки решение.py
+++ 17 задания/17.3 тройки решение.py
@@ -4,12 +4,14 @@ for s in f:
     a.append(int(s))
 
 count = 0
-minar = 10 ** 10
+min_sr_ar = 10 ** 10
 for i in range(len(a) - 2):
     if (abs(a[i]) % 14 == 0 or abs(a[i + 1]) % 14 == 0 or abs(a[i + 2]) % 14 == 0) \
             and (abs(a[i]) % 4 == 0 and abs(a[i + 1]) % 4 == 0 and abs(a[i + 2]) % 4 == 0):
         sr = (a[i] + a[i + 1] + a[i + 2]) // 3
-        minar = min(minar, sr)
+        min_sr_ar = min(min_sr_ar, sr)
         count += 1
 
-print(count, minar)
+print(count, min_sr_ar)
+
+# Ответ: 31 -7504
---
+++ 17 задания/17.5 тройки 3-х число.py
@@ -0,0 +1,15 @@
+a = [int(s) for s in open("17 задания/17 файлы/17_5.txt")]
+count = 0
+min_5 = -1
+max_sum = 0
+for x in range(len(a)):
+    if a[x] % 10 == 5 and len(str(a[x])) == 3:
+        min_5 = min(a[x], min_5)
+for i in range(len(a) - 2):
+    if ((len(str(a[i])) == 3) + (len(str(a[i + 1])) == 3) + (len(str(a[i + 2])) == 3)) == 1:
+        summa = a[i] + a[i + 1] + a[i + 2]
+        if summa % min_5 == 0:
+            max_sum = max(max_sum, summa)
+            count += 1
+
+print(count, max_sum)

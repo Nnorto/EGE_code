ea4cb47f791fa48c6c634ba35d2fb9a4069a4c00
artemzaharov <artem4ikaz@icloud.com>
2023-12-28 00:59:43 +0300

    Решение

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-def f(x, n):
-    if n == 0:
-        return 1
-    elif n % 2 == 0:
-        return (x**2)**(n//2)
-    return x * x ** (n - 1)
+a = []
+with open('17-1.txt') as f:
+    for n in f:
+        n = int(n)
+        a.append(n)
+# забыл рассказать о таком способе чтения, так тоже можно, но сложнее
 
-print(f(2, 10))
+k = 0
+minim =20001
+for i in range(len(a) -2):
+    if a[i] < a[i + 1] < a[i + 2]:
+        k += 1
+    minim = min(minim, a[i+2] - a[i])
+print(k, minim)

abd945922970c705d2223abfbcb58d21c2985150
artemzaharov <artem4ikaz@icloud.com>
2023-12-27 14:30:57 +0300

    Решение

--- 17 задания/17.5  3-х знач. число.py
+++ 17 задания/17.5  3-х знач. число.py
@@ -5,9 +5,9 @@ max_sum = 0
 for x in range(len(a)):
     if a[x] % 10 == 5 and len(str(a[x])) == 3:
         min_5 = min(a[x], min_5)
-for i in range(len(a) - 2):
-    if ((len(str(a[i])) == 3) + (len(str(a[i + 1])) == 3) + (len(str(a[i + 2])) == 3)) == 1:
-        summa = a[i] + a[i + 1] + a[i + 2]
+for i in range(len(a) - 1):
+    if ((len(str(a[i])) == 3) + (len(str(a[i + 1])) == 3)) == 1:
+        summa = a[i] + a[i + 1]
         if summa % min_5 == 0:
             max_sum = max(max_sum, summa)
             count += 1

7cc3284da2ef618c5d73dc80237a91a9683db2d4
artemzaharov <artem4ikaz@icloud.com>
2024-03-06 17:46:17 +0300

    Решение

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
-a = list(map(int, open("17 задание/17 файлы/17-1.txt")))
+a = list(map(int, open("17 задание/17 файлы/17_5.txt")))
 
-max_s = -10**10
+max_e = -10**11
+for el in range(len(a)):
+    if a[el] % 10 == 2:
+        max_e = max(max_e, a[el])
+
+jopa = 10 ** 11
 count = 0
-for i in range(len(a)-1):
-    if (abs(a[i]) % 3 == 0) + (abs(a[i + 1]) % 3 == 0) >= 1:
-        s = a[i] + a[i + 1]
-        count += 1
-        max_s = max(max_s, s)
+for i in range(len(a) - 1):
+    if (a[i] % 10 == 0) + (a[i + 1] % 10 == 0) == 1:
+        summ = a[i] + a[i + 1]
+        if summ < max_e:
+            count += 1
+            jopa = min(jopa, summ)
 
-print(count, max_s)
+print(count, jopa)

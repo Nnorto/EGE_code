0257e0cc43d2a1b4ea8e110f3c18c06e9ce28f43
artemzaharov <artem4ikaz@icloud.com>
2024-03-09 14:53:16 +0300

    Решение

--- пробный/март 1 неделя/24 yandex.py
+++ пробный/март 1 неделя/24 yandex.py
@@ -1,19 +1,7 @@
+# халява
 s = open('24.txt').readline()
 s = s.replace('4', 'a')
 s = s.replace('3', 'e')
-m = 0
-i = 0
-for char in s:
-    if (char == 'y' and i % 3 == 0) or \
-       (char == 'a' and i % 3 == 1) or \
-       (char == 'n' and i % 3 == 2) or \
-       (char == 'd' and i % 3 == 3) or \
-       (char == 'e' and i % 3 == 4) or \
-       (char == 'e' and i % 3 == 4):
-        i += 1
-        m = max(i,m)
-    elif char == 'y':
-        i = 1
-    else:
-        i = 0
-print(m)
+m = 'yandex' * 15  # ну да ручками придется цифры вводить, а потом добавлять +"у" и т.д
+
+print(m in s, len(m), '<= answer')

276d8d04b905b1821d1915059718da398072bd0b
artemzaharov <artem4ikaz@icloud.com>
2024-04-09 09:05:24 +0300

    Решение

--- 25 задание/25 из дз.py
+++ 25 задание/25 из дз.py
@@ -1,7 +1,7 @@
-for i in range (258274,258297+1):
+for i in range(258274,258297+1):
     kd=0
     mas= []
-    for d in range(2, i) :
+    for d in range(2, i):
         if i % d == 0:
             kd+=1
             mas.append(d)

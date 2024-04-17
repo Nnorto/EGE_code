c4e422dbdd2e3b2db267d3ea30f3cdea5ce8239e
artemzaharov <artem4ikaz@icloud.com>
2024-04-17 16:38:14 +0300

    Решение

--- main.py
+++ main.py
@@ -1,53 +1,30 @@
-f = open('17 задание/17 файлы/17_31.txt')
+f = open('17 задание/17 файлы/17_32.txt')
 a = list(map(int, f))
 # min6 = 101010101010
 # for x in a:
 #     if abs(x) % 10 == 6:
 #         min6 = min(min6, x)
 
-min15 = max([x for x in a if abs(x) % 100 == 15])
+max19 = max([x for x in a if x % 100 == 19])
 
 count = 0
-mins = 10**10
-
+maxs = -10**10
 for i in range(len(a) - 2):
-    if (1000<=abs(a[i])<=9999) + (1000<=abs(a[i+1])<=9999) + (1000<=abs(a[i+2])<=9999) == 1:
-        s = a[i] + a[i+1] + a[i+2]
-        if s < min15:
-            count += 1
-            mins = min(mins, s)
-
-
-print(count, mins)
-
-
+    if (len(str(a[i])) == 4) + (len(str(a[i + 1])) == 4) + (len(str(a[i + 2])) == 4) == 2:
+            if (a[i] % 3 == 0) + (a[i+1] % 3 == 0) + (a[i+2] % 3 == 0) >= 1:
+                s = a[i] + a[i+1] + a[i+2]
+                if s > max19:
+                    count += 1
+                    maxs = max(maxs, s)
+print(count, maxs)
 
 count = 0
-mins = 10**10
-
+maxs = -10**10
 for i in range(len(a) - 2):
-    if (len(str(abs(a[i]))) == 4) + (len(str(abs(a[i+1]))) == 4) + (len(str(abs(a[i + 2]))) == 4) == 1:
-        s = a[i] + a[i+1] + a[i+2]
-        if s < min15:
-            count += 1
-            mins = min(mins, s)
-
-
-print(count, mins)
-
-
-min7 = -100010
-for x in a:
-    if abs(x) % 100 == 15:
-        min7 = max(min7, x)
-
-count = 0
-mins = 1010
-for i in range(len(a) - 2):
-    if (len(str(abs(a[i]))) == 4)  + (len(str(abs(a[i + 1]))) == 4) + (len(str(abs(a[i + 2]))) == 4) == 1:
-            s = a[i] + a[i+1] + a[i+2]
-            if s < min7:
-                count += 1
-                mins = min(mins, s)
-
-print(count, mins)
+    if (len(str(a[i])) == 4) + (len(str(a[i + 1])) == 4) + (len(str(a[i + 2])) == 4) == 2:
+            if (a[i] % 3 == 0) + (a[i+1] % 3 == 0) + (a[i+2] % 3 == 0) >= 1:
+                s = a[i] + a[i+1] + a[i+2]
+                if s > max19:
+                    count += 1
+                    maxs = max(maxs, s)
+print(count, maxs)

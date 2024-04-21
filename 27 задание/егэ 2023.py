3179dc49db7bb7f2da3d5c87c5ca54d5d611ae4e
artemzaharov <artem4ikaz@icloud.com>
2024-04-21 19:52:28 +0300

    Решение

--- 27 задание/егэ 2023.py
+++ 27 задание/егэ 2023.py
@@ -8,3 +8,18 @@ for i in range(len(a)):
         for q in range(j+k,len(a)):
             minc=min(minc,a[i]+a[j]+a[q])
 print(minc)
+
+f = open("27 файлы/27_B_1_day.txt")
+
+k = int(f.readline())
+N = f.readline()
+a = list(map(int,f))
+
+min1s = min2s = min3s = 10**10
+for i in range(2 * k, len(a)):
+    min1s = min(min1s, a[i - 2 * k])
+    min2s = min(min2s, a[i - k] + min1s)
+    min3s = min(min3s, a[i] + min2s)
+
+print(min3s)
+
--- main.py
+++ main.py
@@ -1,30 +1,8 @@
-f = open('17 задание/17 файлы/17_32.txt')
-a = list(map(int, f))
-# min6 = 101010101010
-# for x in a:
-#     if abs(x) % 10 == 6:
-#         min6 = min(min6, x)
+def f(x, y):
+    if x == y:
+        return 1
+    if x > y:
+        return 0
+    return f(x + 1, y) + f(x * 2, y)
 
-max19 = max([x for x in a if x % 100 == 19])
-
-count = 0
-maxs = -10**10
-for i in range(len(a) - 2):
-    if (len(str(a[i])) == 4) + (len(str(a[i + 1])) == 4) + (len(str(a[i + 2])) == 4) == 2:
-            if (a[i] % 3 == 0) + (a[i+1] % 3 == 0) + (a[i+2] % 3 == 0) >= 1:
-                s = a[i] + a[i+1] + a[i+2]
-                if s > max19:
-                    count += 1
-                    maxs = max(maxs, s)
-print(count, maxs)
-
-count = 0
-maxs = -10**10
-for i in range(len(a) - 2):
-    if (len(str(a[i])) == 4) + (len(str(a[i + 1])) == 4) + (len(str(a[i + 2])) == 4) == 2:
-            if (a[i] % 3 == 0) + (a[i+1] % 3 == 0) + (a[i+2] % 3 == 0) >= 1:
-                s = a[i] + a[i+1] + a[i+2]
-                if s > max19:
-                    count += 1
-                    maxs = max(maxs, s)
-print(count, maxs)
+print (f(4, 8) * f(8, 10) * f(10, 15))

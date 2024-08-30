1442056b6f01e20df70873118898efc61c1be166
artemzaharov <artem4ikaz@icloud.com>
2024-08-30 18:06:41 +0300

    Решение

--- main.py
+++ main.py
@@ -1,44 +1,31 @@
+f = open('24 пример файла')
+c = 0
+for s in f:
+    a = list(map(int, s.split()))
+    a.sort()
+    b = []
+    for i in a:
+        b.append(a.count(i))
+    if b.count(3) == 3 and b.count(1) == 3:
+        sump = 0
+        sumnp = 0
+        for i in range(len(a)):
+            if b[i] == 3:
+                sump += a[i]
+        for i in range(len(a)):
+            if b[i] == 1:
+                sumnp += a[i]
+        if sump ** 2 > sumnp ** 2:
+            c += 1
 
-for n in reversed(range(4, 10000)):  # да да на этом ловят привыкай
-    s = '5' + '2' * n
-    while '52' in s or '2222' in s or '1122' in s:
-        if '52' in s:
-            s = s.replace('52', '11', 1)
-        if '2222' in s:
-            s = s.replace('2222', '5', 1)
-        if '1122' in s:
-            s = s.replace('1122', '25', 1)
-
-    summa = s.count('1') + s.count('2') * 2 + s.count('5') * 5
-
-    # есть еще способ как найти сумму, через функцию map()
-
-    summa = sum(list(map(int, s)))
-    if summa == 64:
-        print(n)
+print(c)
 
 
-from ipaddress import *
-for A in range(16, 25):
-    net = ip_network(f'117.157.2.8/{A}', 0)
-    flag = True
-    for ad in net:
-        ad_bin = bin(int(ad))[2:].zfill(32)
-        if ad_bin[:16].count('1') < ad_bin[16:].count('1'):
-            flag = False
-            break
-    if flag:
-        print(A)
-
-print('или')
-
-for A in range(9):
-    net = ip_network(f'117.157.2.8/{16+A}', 0)
-    flag = True
-    for ad in net:
-        ad_bin = bin(int(ad))[2:].zfill(32)
-        if ad_bin[:16].count('1') < ad_bin[16:].count('1'):
-            flag = False
-            break
-    if flag:
-        print(A)
+for x in range(2030):
+    c = 7 ** 170 + 7 ** 100 - x
+    s = ""
+    while c > 0:
+        s = str(c % 7) + s
+        c //= 7
+    if s.count('0') == 71:
+        print(x)
